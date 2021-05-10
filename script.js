99c618e65f5226f1331e899c4e2422dbb820f17b
olivia hatcher <hatcher23232@lcsd2kids.org>
2021-05-10 11:41:28 -0600

    center grid

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 const gridContanier = document.getElementById("grid-contanier");
 let userInput = 16;
-for (let index = 0; index < userInput; index++) {
-    let rowCon = document.createElement("div")
 
-    for(let i = 0;i <userInput;i++){ 
-    const singleCell = document.createElement("div");
-    singleCell.textContent = ".";
-    singleCell.style.color = "bg-red";
-    singleCell.classList.add("bg-red");
-singleCell.addEventListener('mouseover' , () => {       
-        singleCell.classList.add= "bg-blue";
-        singleCell.style.color = "bg-blue";
-    });
-rowCon.appendChild(singleCell);
-}   
-gridContanier.appendChild(rowCon);
+
+function bildgrid(){
+    for (let index = 0; index < userInput; index++) {
+        let rowCon = document.createElement("div")
+        for(let i = 0;i<userInput;i++){ 
+        const singlecell = document.createElement("div");
+        singlecell.textContent = ".";
+        singlecell.style.color = "bg-red";
+        singlecell.classList.add("bg-red");
+        singlecell.addEventListener('mouseover' , () => {       
+            singlecell.classList.add ("bg-blue");
+            singlecell.style.color = "bg-blue";
+        });
+        rowCon.appendChild(singlecell);
+    }   
+    gridContanier.appendChild(rowCon);
+    }
 }
+
+document.getElementById("btn").addEventListener("click",() =>{
+    userInput=prompt("hi");
+    gridContanier.innerHTML="";
+    bildgrid();
+
+})
+
+bildgrid();
+
